fdbd1896dcd19657ce56a331c2f96bf9e66097f5
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-02 18:24:08 +0530

    subset sum

---
+++ Aditya Verma/Dynamic Programming/SubsetSumusingTabulation.cpp
@@ -0,0 +1,19 @@
+//There are six variations of problems of knapsack
+// 1.Subset Sum
+// 2.Equal sum partition
+// 3.Count of subset sum
+// 4.Minimum subset sum difference
+// 5.Target sum
+// 6.Number of subsets with given difference
+//To identify whether a problem is a variation of knapsack,
+// an array of different items is given in input , and a bag of particular capacity is given
+
+// SUBSET SUM PROBLEM
+// 1. Problem statement says that we are given an array , we have to check if the sum of
+// any subset is equal to a given number.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    return 0;
+}
