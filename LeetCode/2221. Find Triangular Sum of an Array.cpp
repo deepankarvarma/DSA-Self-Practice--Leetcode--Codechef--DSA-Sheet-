34203642df5340168ba70bf9526254baeab2f949
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-02 23:10:27 +0530

    2221

---
+++ LeetCode/2221. Find Triangular Sum of an Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findsum(vector<int> &nums)
+    {
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            int k = 0;
+            for(int j=0;j<nums.size()-i-1;j++)
+            {
+                nums[k++] = (nums[j]+nums[j+1])%10;
+            }
+        }
+        return nums[0];
+    }
+    
+    int triangularSum(vector<int>& nums) {
+        if(nums.size()==1)
+        {
+            return nums[0];
+        }
+        int n = findsum(nums);
+        return n;
+    }
+};
