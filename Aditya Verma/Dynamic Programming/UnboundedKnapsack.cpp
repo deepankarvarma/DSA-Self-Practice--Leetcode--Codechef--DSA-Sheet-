af30ee32729481f4e3ea3129fcb888e56950d8f6
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-03 15:44:01 +0530

    unbounded knapsack

---
+++ Aditya Verma/Dynamic Programming/UnboundedKnapsack.cpp
@@ -0,0 +1,10 @@
+// Main difference is that multiple occurences of same item is allowed that means
+// we can choose a item any number of times.
+// Related problems:-
+// 1)Rod cutting
+// 2)Coin change I (Maximum number of ways)
+// 3)Coin change II (Minimum numebr of coins)
+// 4)Maximum ribbon cut
+// The main use of multiple occurences is that if a item is rejected then it will always be 
+// rejected and this process is completed.But if a item is accepted now may be in future
+// that may be rejected and hence the process is not complete in just 1 operation
