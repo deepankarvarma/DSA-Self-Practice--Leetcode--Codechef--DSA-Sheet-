5ec9603188cd9320c318f07875cc08adcb7097f8
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-05 11:06:24 +0530

    intro lcs

---
+++ Aditya Verma/Dynamic Programming/Longest Common Subsequence/Introduction.cpp
@@ -0,0 +1,15 @@
+// Maximum number of problems :-
+// 1) Largest Common Substring
+// 2) Print LCS
+// 3) Shortest Common Supersequence
+// 4) Print SCS
+// 5) Minimum # of insertion and deletions a->b
+// 6) Longest repeating subsequence
+// 7) Length of longest subsequence of a which is a substring in b
+// 8) Subsequence pattern matching
+// 9) Count of times a appears as a subsequence in b
+// 10) Longest Palindromic Subsequence
+// 11) Longest Palindromic Substring
+// 12) Count of palindromic substring
+// 13) Min # of deletions in a string to make it a palindrome
+// 14) Min # of insertions in a string to make it a palindrome
