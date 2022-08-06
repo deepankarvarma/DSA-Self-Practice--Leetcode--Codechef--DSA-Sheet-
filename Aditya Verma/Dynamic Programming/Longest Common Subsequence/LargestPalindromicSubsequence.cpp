bd91e391c1955ed89f984948211ce8924a9ce69c
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-06 12:03:26 +0530

    LPS

---
+++ Aditya Verma/Dynamic Programming/Longest Common Subsequence/LargestPalindromicSubsequence.cpp
@@ -0,0 +1,30 @@
+// I/P:- Single string; O/P :-We have to return the length of the largest palindromic subsequence inside this string
+//We have to make another string which is the reverse of innput string & then 
+// find the LCS between two.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a="bbbab";
+    string b="";
+    for(int i=a.length()-1;i>=0;i--){
+        b+=a[i];
+    }
+    int n=a.length();
+    int t[n+1][n+1];//both string have same length hence we have used 1 variable only
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<n+1;j++){
+            if(i==0 || j==0)
+            t[i][j]=0;
+        }
+    }
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<n+1;j++){
+            if(a[i-1]==b[j-1])
+            t[i][j]=1+t[i-1][j-1];
+            else
+            t[i][j]=max(t[i-1][j],t[i][j-1]);
+        }
+    }
+    cout<<t[n][n];
+    return 0;
+}
