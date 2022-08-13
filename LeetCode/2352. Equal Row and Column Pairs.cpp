e3b31fa610312763641bb7fe15c4c4b4ea91d956
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-13 09:43:52 +0530

    2352

---
+++ LeetCode/2352. Equal Row and Column Pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+	int equalPairs(vector<vector<int>>& grid) {
+		int n=grid.size(),count=0;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				bool f=true;
+				for(int k=0;k<n;k++){
+					if(grid[k][j]!=grid[i][k]){
+						f=false;
+						break;
+					}    
+				}
+				if(f)count++;
+			}
+		}
+		return count;
+	}
+};
