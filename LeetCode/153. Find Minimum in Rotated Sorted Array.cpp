20d29b625b385845e5acd09041f49a621de7285c
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-14 13:37:58 +0530

    153

---
+++ LeetCode/153. Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countRotations(vector<int>& arr, int low, int high)
+{
+	if(low>high)
+        return 0;
+    if (high == low)
+		return low;
+    int mid = low + (high - low) / 2; /*(low + high)/2;*/
+    if (mid < high && arr[mid + 1] < arr[mid])
+		return (mid + 1);
+    if (mid > low && arr[mid] < arr[mid - 1])
+		return mid;
+    if (arr[high] > arr[mid])
+		return countRotations(arr, low, mid - 1);
+
+	return countRotations(arr, mid + 1, high);
+}
+    int findMin(vector<int>& nums) {
+    return nums[countRotations(nums, 0, nums.size() - 1)];
+    }
+};
