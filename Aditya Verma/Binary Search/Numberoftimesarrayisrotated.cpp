a550caa815c331b769451b788d9131df89b637af
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-14 10:59:23 +0530

    number of times a array is rotated

---
+++ Aditya Verma/Binary Search/Numberoftimesarrayisrotated.cpp
@@ -0,0 +1,6 @@
+// Consider an array of distinct numbers sorted in increasing order.
+//  The array has been rotated (clockwise) k number of times. Given such an array, 
+// find the value of k.
+
+// Approach : The value of K will be equal to the index of minimum element
+2 3 4 5 6 7 8 => 3 4 5 6 7 8 2
