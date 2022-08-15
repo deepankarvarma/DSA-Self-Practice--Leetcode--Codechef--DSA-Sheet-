fe98d997365aeec78c854d5d2878722e4566b9ac
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-15 18:11:39 +0530

    some

--- Codechef/1.cpp
+++ Codechef/1.cpp
@@ -1,48 +1,7 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Edge {
-    int src, dest;
-};
-
-class Graph
-{
-public:
-   
-    vector<vector<int>> adjList;
-  Graph(vector<Edge> const &edges, int n)
-    {
-      
-        adjList.resize(n);
- 
-        for (auto &edge: edges)
-        {
-
-            adjList[edge.src].push_back(edge.dest);
-        }
-    }
-};
-
-
-int main()
-{
-    vector<Edge> edges;
-    int n ;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            Edge e;
-            e.src=j;
-            e.dest=i;
-           if(i>j && (i+1)%(j+1)==0){ 
-            
-            edges.push_back(e);
-            }
-        }
-    }
-    Graph graph(edges, n);
-    
- 
+int main(){
+    cout<<5 & 6;
     return 0;
 }
