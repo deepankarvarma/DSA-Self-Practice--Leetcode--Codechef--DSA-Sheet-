332458493cd90104f543016db5183c876cf407f3
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-18 17:21:02 +0530

    724

---
+++ LeetCode/724. Find Pivot Index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+      int leftSum = 0;
+        int sum = 0;
+        for(auto num:nums){
+            sum += num;
+        }
+        for(int i=0;i<nums.size();i++){
+            sum -= nums[i];
+            if(leftSum == sum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
