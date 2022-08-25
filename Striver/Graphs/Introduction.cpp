9e9580983457bc7789af416dc307a04012c3ca32
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-25 18:19:31 +0530

    intro

---
+++ Striver/Graphs/Introduction.cpp
@@ -0,0 +1,13 @@
+// Two types of graphs:
+// 1. Directed Graphs
+// 2. Undirected Graphs
+// Directed graphs have directed edges means u->v != v->u whereas in undirected graphs
+// u->v ==v->u
+
+// In undirected graphs only degree is there, whereas in directed graphs concept of indegree
+// and outdegree is there 
+
+// Sum of all degrees in undirected graph = 2* number of edges 
+// In a path we cannot visit a vertex more than once
+// A graph is called cyclic if we can reach a  vertex again by traveling.
+
