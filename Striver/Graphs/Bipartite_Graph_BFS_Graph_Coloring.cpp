3fd36f307e361957bd10d96f25ea42725fd8bcfc
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-26 11:08:46 +0530

    1791

---
+++ Striver/Graphs/Bipartite_Graph_BFS_Graph_Coloring.cpp
@@ -0,0 +1,5 @@
+// Bipartite graph is a graph that can be coloured using 2 colours such that no two
+// adjacent nodes have same colour.
+// Any graph having cycle of odd length can never be bipartite.
+// Any graph doesn't having any odd length cycle is bipartite.It is not necessary that the cycle length is even
+// even if the cycle is absent then also it can be bipartite.
