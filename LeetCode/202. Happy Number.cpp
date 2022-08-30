52b8249a8d3e354f4013726b0f60c5f3b65c7e19
deepankarvarma <satwikdpshrit@gmail.com>
2022-08-30 22:58:59 +0530

    202

---
+++ LeetCode/202. Happy Number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+int temp = n;
+        while(true){
+            if(temp==89){
+                return false;
+            }else if(temp == 1){
+                return true;
+            }else{
+                int sum = 0;
+                while(temp>0){
+            int digi = temp%10;
+            sum += digi*digi;
+            temp /= 10;
+                }
+                temp=sum;
+            
+            }
+        }
+    }
+};
