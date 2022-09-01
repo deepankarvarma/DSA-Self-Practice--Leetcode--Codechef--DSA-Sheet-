04db051e7183dd1af2db3a16dc8fbb61c7cdf836
deepankarvarma <satwikdpshrit@gmail.com>
2022-09-01 20:29:44 +0530

    1769

--- LeetCode/1769. Minimum Number of Operations to Move All Balls to Each Box.cpp
+++ LeetCode/1769. Minimum Number of Operations to Move All Balls to Each Box.cpp
@@ -1,30 +1,18 @@
+// 1769. Minimum Number of Operations to Move All Balls to Each Box
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        int i=0;
-        int j=0;
-        vector<int> ans;
-        int sum=0;
-        cout<<boxes.length()-1;
-        while(i<boxes.length() && j<boxes.length()){
-            if(j==boxes.length()-1){
-                if(boxes[j]=='1')
-                    sum+=abs(i-j);
-                
-                ans.push_back(sum);
-                i++;
-                j=0;
-                sum=0;
-            }
-            else if(boxes[j]=='1'){
-                sum+=abs(i-j);
-                cout<<sum<<j;
-                j++;
-            }
-            else {
-                j++;
+         vector<int> arr(boxes.length(),0);
+        for(int i=0;i<boxes.size();i++)
+        {
+            for(int j=0;j<boxes.size();j++)
+            {
+                if(boxes[j] == '1')
+                {
+                    arr[i] += abs(i-j);
+                }
             }
         }
-        return ans;
+        return arr;
     }
 };
