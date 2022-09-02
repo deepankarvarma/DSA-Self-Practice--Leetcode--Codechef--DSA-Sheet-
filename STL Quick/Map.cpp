f1ea2313b06d76e4fe5c2be02df1a9ce1207c255
deepankarvarma <satwikdpshrit@gmail.com>
2022-09-02 21:51:14 +0530

    map

---
+++ STL Quick/Map.cpp
@@ -0,0 +1,22 @@
+// A-> B and A -> C is not possible, but A-> B and C-> B is possible.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // unordered_map<int,string> m;
+    map<int,string> m;
+    m[1]="love";
+    m[13]="deeps";
+    m[2]="babbar";
+    m.insert({5,"bheem"});
+    for(auto i:m){
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+    
+    cout<<m.count(13)<<endl;
+    m.erase(13);
+     auto it=m.find(2);//just returns the iterator
+    for(auto i=it;i!=m.end();i++){
+        cout<<(*i).first<<endl;
+    }
+    return 0;
+}
