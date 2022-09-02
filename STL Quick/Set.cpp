4a9f09794a6d76f57cdb1ee0ee323e71e9071823
deepankarvarma <satwikdpshrit@gmail.com>
2022-09-02 20:16:26 +0530

    set done

--- STL Quick/Set.cpp
+++ STL Quick/Set.cpp
@@ -16,11 +16,15 @@ int main(){
     s.insert(0);
     for(int i:s)
     cout<<i<<endl;
+    cout<<endl;
     set<int>::iterator it=s.begin();
     it++;
     s.erase(it);
     for(int i:s)
     cout<<i<<endl;
     // cout<<s.begin()<<endl; This does not works
+    cout<<endl;
+    cout<<s.count(5)<<endl;
+    cout<<s.count(3)<<endl;  //check whether 5 is present in the set or not
     return 0;
 }
