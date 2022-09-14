691940b00ce19b24f8e6d01ed4d4e7f3ce42fb7a
deepankarvarma <satwikdpshrit@gmail.com>
2022-09-14 21:28:44 +0530

    1672

--- LeetCode/1672. Richest Customer Wealth.cpp
+++ LeetCode/1672. Richest Customer Wealth.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int m=0;
-        int lm=0;
-       for(int i=0;i<accounts.size();i++){
-           for(int j=0;j<accounts[i].size();j++){
-               lm+=accounts[i][j];
-              
-           }
-           if(m<lm)
-               m=lm;
-           lm=0;
-       }
-        
-        
-        return m;
+        int maxs=INT_MIN;
+        for(int i=0;i<accounts.size();i++){
+            int temp=accumulate(accounts[i].begin(),accounts[i].end(),0);
+            maxs=max(maxs,temp);
+        }
+        return maxs;
     }
 };
