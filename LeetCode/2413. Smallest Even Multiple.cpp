f52ef1926c34c9d1618dc140147964e2b1c4df01
deepankarvarma <satwikdpshrit@gmail.com>
2022-11-09 22:59:38 +0530

    2413

---
+++ LeetCode/2413. Smallest Even Multiple.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if(n==0)
+            return 0;
+        else{
+            if(n%2==0)
+                return n;
+            else 
+                return 2*n;
+        }
+    }
+};
