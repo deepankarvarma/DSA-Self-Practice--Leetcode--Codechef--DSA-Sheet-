52df95ba0b089cf15edb71cc71a7a0d022f671c2
deepankarvarma <satwikdpshrit@gmail.com>
2022-12-28 11:17:51 +0530

    ll

--- Codechef/1.cpp
+++ Codechef/1.cpp
@@ -1,39 +1,40 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <bits/stdc++.h>
-
 using namespace std;
-
+int ans = 100000000;
+void solve(int a[], int n, int k, int index, int sum,
+		int maxsum)
+{
+	if (k == 1) {
+		maxsum = max(maxsum, sum);
+		sum = 0;
+		for (int i = index; i < n; i++) {
+			sum += a[i];
+		}
+		maxsum = max(maxsum, sum);
+		ans = min(ans, maxsum);
+		return;
+	}
+	sum = 0;
+	for (int i = index; i < n; i++) {
+		sum += a[i];
+		maxsum = max(maxsum, sum);
+		solve(a, n, k - 1, i + 1, sum, maxsum);
+	}
+}
 int main()
 {
-    vector<int> direction={1,-1,1};
-    vector<int> strength={5,3,1};
-     stack<int> s;
-        for (int i = 0; i < direction.size(); i++) {
-            if (direction[i] > 0 || s.empty())
-                s.push(i);
-            else {
-                while (!s.empty() && direction[s.top()] > 0 && strength[i] > strength[s.top()])
-                    s.pop();
-                if (s.empty() || direction[s.top()] < 0)
-                    s.push(i);
-                else if (strength[s.top()] == strength[i])
-                    s.pop();
-            }
-        }
-        vector<int> res;
-        while(!s.empty()){
-            res.push_back(s.top());
-            s.pop();
-        }
-        reverse(res.begin(),res.end());
-        return res;
-        for(auto i:res)
-         cout<<i;
+    int t;
+    cin>>t;
+    while(t--){
+    int n,k;
+    cin>>n>>k;
+	int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+	solve(arr, n, k, 0, 0, 0);
+	cout << ans << "\n";
+    }
+    return 0;
 }
